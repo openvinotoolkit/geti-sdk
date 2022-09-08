9162d8bf20224b6ff34006f2518ce6f9db019799
ljcornel <ludo.cornelissen@intel.com>
2022-09-08 13:30:15 +0200

    Fix deployment file structure when created with sdk

--- sc_api_tools/deployment/deployed_model.py
+++ sc_api_tools/deployment/deployed_model.py
@@ -34,6 +34,7 @@ from sc_api_tools.http_session import SCSession
 from sc_api_tools.rest_converters import ConfigurationRESTConverter, ModelRESTConverter
 
 MODEL_DIR_NAME = "model"
+PYTHON_DIR_NAME = "python"
 WRAPPER_DIR_NAME = "model_wrappers"
 
 LABELS_CONFIG_KEY = "labels"
@@ -80,43 +81,54 @@ class DeployedModel(OptimizedModel):
         """
         if isinstance(source, (os.PathLike, str)):
             if os.path.isfile(source) and os.path.splitext(source)[1] == ".zip":
+                # Extract zipfile into temporary directory
                 if self._model_data_path is None:
-                    model_dir = tempfile.mkdtemp()
+                    temp_dir = tempfile.mkdtemp()
                     self._needs_tempdir_deletion = True
                 else:
-                    model_dir = self._model_data_path
+                    temp_dir = self._model_data_path
+
                 with zipfile.ZipFile(source, "r") as zipped_source_model:
-                    zipped_source_model.extractall(model_dir)
+                    zipped_source_model.extractall(temp_dir)
 
-                self._model_data_path = os.path.join(model_dir, MODEL_DIR_NAME)
-                self._model_python_path = os.path.join(self._model_data_path, "python")
+                # _model_data_path contains the model structure and weights
+                # _model_python_path contains the custom model wrappers
+                self._model_data_path = os.path.join(temp_dir, MODEL_DIR_NAME)
+                self._model_python_path = os.path.join(temp_dir, PYTHON_DIR_NAME)
 
-                # Check if the model includes custom model wrappers, if so include them
-                # in the data dir
-                python_path = os.path.join(model_dir, "python")
-                if WRAPPER_DIR_NAME in os.listdir(python_path):
-                    wrappers_path = os.path.join(python_path, WRAPPER_DIR_NAME)
-                    wrappers_destination_path = os.path.join(
-                        self._model_data_path, WRAPPER_DIR_NAME
-                    )
-                    shutil.copytree(src=wrappers_path, dst=wrappers_destination_path)
+                # Check if the model includes custom model wrappers
+                if WRAPPER_DIR_NAME in os.listdir(self._model_python_path):
                     self._has_custom_model_wrappers = True
-                self.get_data(self._model_data_path)
+                self.get_data(temp_dir)
 
             elif os.path.isdir(source):
-                if MODEL_DIR_NAME in os.listdir(source):
-                    source = os.path.join(source, MODEL_DIR_NAME)
                 source_contents = os.listdir(source)
-                if "model.bin" in source_contents and "model.xml" in source_contents:
-                    self._model_data_path = source
+                if MODEL_DIR_NAME in source_contents:
+                    model_dir = os.path.join(source, MODEL_DIR_NAME)
+                else:
+                    model_dir = source
+                model_dir_contents = os.listdir(model_dir)
+                if (
+                    "model.bin" in model_dir_contents
+                    and "model.xml" in model_dir_contents
+                ):
+                    self._model_data_path = model_dir
                 else:
                     raise ValueError(
-                        f"Unable to load model data from path '{source}'. Model "
+                        f"Unable to load model data from path '{model_dir}'. Model "
                         f"file 'model.xml' and weights file 'model.bin' were not found "
                         f"at the path specified. "
                     )
-                if WRAPPER_DIR_NAME in source_contents:
+                if PYTHON_DIR_NAME in source_contents:
+                    model_python_path = os.path.join(source, PYTHON_DIR_NAME)
+                else:
+                    model_python_path = os.path.join(
+                        os.path.dirname(source), PYTHON_DIR_NAME
+                    )
+                python_dir_contents = os.listdir(model_python_path)
+                if WRAPPER_DIR_NAME in python_dir_contents:
                     self._has_custom_model_wrappers = True
+                    self._model_python_path = os.path.join(source, PYTHON_DIR_NAME)
 
         elif isinstance(source, SCSession):
             if self.base_url is None:
