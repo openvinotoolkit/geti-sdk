8626479ba86419f64e444e18ebe1cdd31ff0bdab
ljcornel <ludo.cornelissen@intel.com>
2022-09-30 14:52:12 +0200

    Add deserialization test to utils integration tests

--- tests/integration/utils/test_utils.py
+++ tests/integration/utils/test_utils.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions
 # and limitations under the License.
+import copy
 
 import pytest
 
-from geti_sdk.data_models import Algorithm, TaskType
-from geti_sdk.utils import get_supported_algorithms
+from geti_sdk.data_models import Algorithm, Project, TaskType
+from geti_sdk.utils import deserialize_dictionary, get_supported_algorithms
+from geti_sdk.utils.serialization_helpers import DataModelMismatchException
 
 
 class TestUtils:
@@ -47,3 +49,32 @@ class TestUtils:
         assert len(classification_algos) > 0
         for algorithm in classification_algos:
             assert algorithm.task_type == TaskType.CLASSIFICATION
+
+    def test_deserialize_dictionary(self, fxt_project_dictionary: dict):
+        """
+        Verifies that deserializing a dictionary to a python object works.
+
+        Also tests that a DataModelMismatchException is raised in case:
+            1. the input dictionary contains an invalid key
+            2. the input dictionary misses a required key
+        """
+        object_type = Project
+        project = deserialize_dictionary(
+            input_dictionary=fxt_project_dictionary, output_type=object_type
+        )
+        assert project.name == fxt_project_dictionary["name"]
+        assert project.get_trainable_tasks()[0].type == TaskType.DETECTION
+
+        dictionary_with_extra_key = copy.deepcopy(fxt_project_dictionary)
+        dictionary_with_extra_key.update({"invalid_key": "invalidness"})
+        with pytest.raises(DataModelMismatchException):
+            deserialize_dictionary(
+                input_dictionary=dictionary_with_extra_key, output_type=object_type
+            )
+
+        dictionary_with_missing_key = copy.deepcopy(fxt_project_dictionary)
+        dictionary_with_missing_key.pop("pipeline")
+        with pytest.raises(DataModelMismatchException):
+            deserialize_dictionary(
+                input_dictionary=dictionary_with_extra_key, output_type=object_type
+            )
