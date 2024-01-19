8cd588f04a7ebba35f063bbb8325e30a021cd33b
Igor Davidyuk <igor.davidyuk@intel.com>
2024-01-19 19:53:47 +0100

    add nightly test
    Signed-off-by: Igor Davidyuk <igor.davidyuk@intel.com>

--- tests/nightly/test_classification.py
+++ tests/nightly/test_classification.py
@@ -12,14 +12,53 @@
 # See the License for the specific language governing permissions
 # and limitations under the License.
 
+import pandas as pd
 from test_nightly_project import TestNightlyProject
+
+from geti_sdk.benchmarking.benchmarker import Benchmarker
+from geti_sdk.geti import Geti
+from tests.helpers import project_service
 
 
 class TestClassification(TestNightlyProject):
     """
-    Class to test project creation, annotation upload, training, prediction and
+    Class to test project creation, annotation upload, training, prediction, benchmarking and
     deployment for a classification project
     """
 
     PROJECT_TYPE = "classification"
     __test__ = True
+
+    def test_deployment(
+        self,
+        fxt_project_service_no_vcr: project_service,
+        fxt_geti_no_vcr: Geti,
+        fxt_temp_directory: str,
+        fxt_image_path: str,
+        fxt_image_path_complex: str,
+        fxt_artifact_directory: str,
+    ):
+        """
+        Tests benchmarking for the project.
+        """
+        project = fxt_project_service_no_vcr.project
+        algorithms_to_benchmark = ["EfficientNet-B0", "MobileNet-V3-large-1x"]
+        images = [fxt_image_path, fxt_image_path_complex]
+        precision_levels = ["FP32", "FP16", "INT8"]
+
+        benchmarker = Benchmarker(
+            geti=fxt_geti_no_vcr,
+            project=project,
+            algorithms=algorithms_to_benchmark,
+            precision_levels=precision_levels,
+            benchmark_images=images,
+        )
+        benchmarker.prepare_benchmark(working_directory=fxt_temp_directory)
+        results = benchmarker.run_throughput_benchmark(
+            working_directory=fxt_temp_directory,
+            results_filename="results",
+            target_device="CPU",
+            frames=2,
+            repeats=2,
+        )
+        pd.DataFrame(results)
