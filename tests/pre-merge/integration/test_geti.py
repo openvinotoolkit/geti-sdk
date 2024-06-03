2b4906c1df7f5bbbd2f2c5c3a386e54cd138d71d
Igor Davidyuk <igor.davidyuk@intel.com>
2024-06-03 16:08:15 +0200

    fix integration test
    Signed-off-by: Igor Davidyuk <igor.davidyuk@intel.com>

--- tests/pre-merge/integration/test_geti.py
+++ tests/pre-merge/integration/test_geti.py
@@ -535,8 +535,7 @@ class TestGeti:
 
         if all([model.has_xai_head for model in deployment.models]):
             assert explain_prediction.feature_vector is not None
-
-        assert len(explain_prediction.maps) > 0
+            assert len(explain_prediction.maps) > 0
 
         online_mask = online_prediction.as_mask(image.media_information)
         local_mask = local_prediction.as_mask(image.media_information)
