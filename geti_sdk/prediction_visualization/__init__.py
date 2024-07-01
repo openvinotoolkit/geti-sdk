a4f03d72d196c0442766c1b1a359d82e8e826230
Igor Davidyuk <igor.davidyuk@intel.com>
2024-07-01 20:08:08 +0200

    expand visualizer docs
    Signed-off-by: Igor Davidyuk <igor.davidyuk@intel.com>

--- geti_sdk/prediction_visualization/__init__.py
+++ geti_sdk/prediction_visualization/__init__.py
@@ -16,8 +16,48 @@
 Introduction
 ------------
 
-The `prediction_visualization` package provides classes for visualizing models predictions.
-Currently, the user interfaces to this package are available in the :py:mod:`~geti_sdk.utils.plot_helpers` module.
+The `prediction_visualization` package provides classes for visualizing models predictions and media annotations.
+Aditionally, shortend interface to this package is available through the :py:mod:`~geti_sdk.utils.plot_helpers` module.
+
+The main :py:class:`~geti_sdk.prediction_visualization.visualizer.Visualizer` class is a flexible utility class for working
+with Geti-SDK Prediction and Annotation object. You can initialize the Visualizer with the desired settings and then use it to draw
+the annotations on the input image.
+
+.. code-block:: python
+
+   from geti_sdk import Visualizer
+
+   visualizer = Visualizer(
+      show_labels=True,
+      show_confidence=True,
+      show_count=False,
+   )
+
+   # Obtain a prediction from the Intel Geti platfor server or a local deployment.
+   ...
+
+   # Visualize the prediction on the input image.
+   result = visualizer.draw(
+      numpy_image,
+      prediction,
+      fill_shapes=True,
+      confidence_threshold=0.4,
+   )
+   visualizer.show_in_notebook(result)
+
+In case the Prediction was generated with a model that supports explainable AI functionality, the Visualizer can also display
+the explanation for the prediction.
+
+.. code-block:: python
+   image_with_saliency_map = visualizer.explain_label(
+      numpy_image,
+      prediction,
+      label_name="Cat",
+      opacity=0.5,
+      show_predictions=True,
+   )
+   visualizer.save_image(image_with_saliency_map, "./explained_prediction.jpg")
+   visualizer.show_window(image_with_saliency_map)  # When called in a script
 
 Module contents
 ---------------
