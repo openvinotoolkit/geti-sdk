abe50cc0835a4bff63b10428bc38b0f28f12dc1f
Igor Davidyuk <igor.davidyuk@intel.com>
2024-07-30 17:14:44 +0200

    update tests
    Signed-off-by: Igor Davidyuk <igor.davidyuk@intel.com>

--- tests/pre-merge/unit/rest_clients/test_credit_system_client_unit.py
+++ tests/pre-merge/unit/rest_clients/test_credit_system_client_unit.py
@@ -38,14 +38,14 @@ class TestCreditSystemClient:
         # 2. Act and Assert
         # The CS client fires a balance call to check if the CS enabled on the server
         # If the CS enabled, the server returns a message that is decoded to a dict
-        balance_response = {"available": 10}
+        balance_response = {"available": 10, "incoming": 20, "blocked": 5}
         mocker.patch.object(
             mock_session, "get_rest_response", return_value=balance_response
         )
         cs_client = CreditSystemClient(
             session=mock_session, workspace_id=mock_workspace_id
         )
-        assert cs_client.get_balance() == balance_response["available"]
+        assert cs_client.get_balance().available == balance_response["available"]
 
         # If the CS is disabled, the server returns an empty 200 response
         empty_response = Response()
